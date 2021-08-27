e5b66190ed2a95928dcd3134f5a14339cdbbafcf
Muhammad Sarim Khan Bangash <bangashsarim@gmail.com>
2021-08-27 15:16:38 +0500

    React-Food-Menu-App

---
+++ src/components/Menu.jsx
@@ -0,0 +1,84 @@
+const Menu = [
+  {
+    id: 1,
+    image:
+      "https://photo.foodgawker.com/wp-content/uploads/2018/07/3266399.jpg",
+    name: "maggi",
+    category: "breakfast",
+    price: "12rs",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+
+  {
+    id: 2,
+    image:
+      "https://ikneadtoeat.com/wp-content/uploads/2019/09/Aloo-Pakora-Recipe-1.jpg",
+    name: "allu pakoda",
+    category: "evening",
+    price: "20rs",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    id: 3,
+    image:
+      "https://static.fanpage.it/wp-content/uploads/sites/22/2020/06/Pannocchie-al-cartoccio-638x425.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "10rs",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    id: 4,
+    image: "https://kfoods.com/images1/newrecipeicon/cholay-chaat_5613.jpg",
+    name: "chola",
+    category: "lunch",
+    price: "50rs",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    id: 5,
+    image: "https://static.toiimg.com/photo/53110049.cms",
+    name: "pizza",
+    category: "evening",
+    price: "80rs",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    id: 6,
+    image:
+      "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/1/2021_1$largeimg_1922831586.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "180rs",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    id: 7,
+    image:
+      "https://media.istockphoto.com/photos/indian-sweets-in-a-plate-includes-gulab-jamun-rasgulla-kaju-katli-picture-id1054228718?k=6&m=1054228718&s=612x612&w=0&h=3qLVD0_7pSfR7MZ1bwoyO-vJJ0GpDENwoU7J0r0Pn14=",
+    name: "Sweets",
+    category: "dinner",
+    price: "60rs",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    id: 8,
+    image:
+      "https://www.indianfoodforever.com/iffwd/wp-content/uploads/rajma-chawal.jpg",
+    name: "Rajma Rice",
+    category: "lunch",
+    price: "60rs",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    id: 9,
+    image:
+      "https://static.toiimg.com/thumb/61050397.cms?imgsize=246859&width=800&height=800",
+    name: "samosa",
+    category: "evening",
+    price: "10rs",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+];
+
+export default Menu;
